5cb6e156fceba6243c482120c8b980dbcd75b3d5
Iaroslav Sorokin <iaroslav.sorokin@epitech.eu>
2020-02-02 21:31:04 +0100

    Added other patterns.

--- sources/builtin_pattern.c
+++ sources/builtin_pattern.c
@@ -56,20 +56,52 @@ nfa_node_t *bi_exit_pattern()
     opts[4] = (node_opt_t){0, NFA_DEFAULT | NFA_LOOPED, (int[]){2, 0}, 3};
     opts[4] = (node_opt_t){" \n", NFA_FINAL, (int[]){2, 0}, 3};
     opts[5] = (node_opt_t){0, NFA_FINAL | NFA_LOOPED, (int[]){3, 0, 1}, 4};
-    return (create_expression(opts, 5));
+    return (create_expression(opts, 6));
 }
 
 nfa_node_t *bi_setenv_pattern()
 {
-    return (0);
+    node_opt_t opts[9];
+
+    opts[0] = (node_opt_t){"s", NFA_DEFAULT, (int[]){2, 0}, -1};
+    opts[1] = (node_opt_t){"e", NFA_DEFAULT, (int[]){2, 0}, 0};
+    opts[2] = (node_opt_t){"t", NFA_DEFAULT, (int[]){2, 0}, 1};
+    opts[3] = (node_opt_t){"e", NFA_DEFAULT, (int[]){2, 0}, 2};
+    opts[4] = (node_opt_t){"n", NFA_DEFAULT, (int[]){2, 0}, 3};
+    opts[5] = (node_opt_t){"v", NFA_FINAL, (int[]){2, 0}, 4};
+    opts[6] = (node_opt_t){0, NFA_DEFAULT | NFA_LOOPED, (int[]){2, 0}, 5};
+    opts[7] = (node_opt_t){" \n", NFA_FINAL, (int[]){2, 0}, 5};
+    opts[8] = (node_opt_t){0, NFA_FINAL | NFA_LOOPED, (int[]){3, 0, 1}, 7};
+    return (create_expression(opts, 9));
 }
 
 nfa_node_t *bi_env_pattern()
 {
-    return (0);
+    node_opt_t opts[6];
+
+    opts[0] = (node_opt_t){"e", NFA_DEFAULT, (int[]){2, 0}, -1};
+    opts[1] = (node_opt_t){"n", NFA_DEFAULT, (int[]){2, 0}, 0};
+    opts[2] = (node_opt_t){"v", NFA_FINAL, (int[]){2, 0}, 1};
+    opts[3] = (node_opt_t){0, NFA_DEFAULT | NFA_LOOPED, (int[]){2, 0}, 2};
+    opts[4] = (node_opt_t){" \n", NFA_FINAL, (int[]){2, 0}, 2};
+    opts[5] = (node_opt_t){0, NFA_FINAL | NFA_LOOPED, (int[]){3, 0, 1}, 4};
+    return (create_expression(opts, 6));
 }
 
 nfa_node_t *bi_unsetenv_pattern()
 {
-    return (0);
+    node_opt_t opts[11];
+
+    opts[0] = (node_opt_t){"u", NFA_DEFAULT, (int[]){2, 0}, -1};
+    opts[1] = (node_opt_t){"n", NFA_DEFAULT, (int[]){2, 0}, 0};
+    opts[2] = (node_opt_t){"s", NFA_DEFAULT, (int[]){2, 0}, 1};
+    opts[3] = (node_opt_t){"e", NFA_DEFAULT, (int[]){2, 0}, 2};
+    opts[4] = (node_opt_t){"t", NFA_DEFAULT, (int[]){2, 0}, 3};
+    opts[5] = (node_opt_t){"e", NFA_DEFAULT, (int[]){2, 0}, 4};
+    opts[6] = (node_opt_t){"n", NFA_DEFAULT, (int[]){2, 0}, 5};
+    opts[7] = (node_opt_t){"v", NFA_FINAL, (int[]){2, 0}, 6};
+    opts[8] = (node_opt_t){0, NFA_DEFAULT | NFA_LOOPED, (int[]){2, 0}, 7};
+    opts[9] = (node_opt_t){" \n", NFA_FINAL, (int[]){2, 0}, 7};
+    opts[10] = (node_opt_t){0, NFA_FINAL | NFA_LOOPED, (int[]){3, 0, 1}, 9};
+    return (create_expression(opts, 11));
 }
