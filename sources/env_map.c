888db21fa874eee5bf024a5b07e29ddceb26e9a9
Iaroslav Sorokin <iaroslav.sorokin@epitech.eu>
2020-04-26 01:49:53 +0200

    Lose. Didn't make it, sry

--- sources/env_map.c
+++
@@ -1,7 +0,0 @@
-/*
-** EPITECH PROJECT, 2019
-** Title
-** File description:
-** Description
-*/
-#include "env_map.h"
--- sources/redirect_1.c
+++
@@ -1,8 +0,0 @@
-/*
-** EPITECH PROJECT, 2019
-** Title
-** File description:
-** Description
-*/
-
-#include "redirect.h"
